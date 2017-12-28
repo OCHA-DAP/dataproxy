37aaddbc0e520285b9adbb87401657887f065e34
Serban Teodorescu <teodorescu.serban@gmail.com>
2017-12-28 12:46:00 +0200

    added required files to be able to build a docker image on docker hub

---
+++ docker/gunicorn_conf.py
@@ -0,0 +1,9 @@
+bind = '0.0.0.0:5000'
+workers = 4
+#worker_class = 'gevent'
+access_log_format = '"%({x-forwarded-for}i)s" %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+#access_log_format = '"%({x-real-ip}i)s" %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+accesslog = '/var/log/dataproxy.access.log'
+errorlog = '/var/log/dataproxy.error.log'
+loglevel = 'warning'
+#loglevel = 'debug'
